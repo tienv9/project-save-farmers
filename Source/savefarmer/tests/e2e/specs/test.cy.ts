8fd7ff6d5a9c348ffac5537f97e07afcb3238277
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2024-11-27 12:02:35 -0800

    Implement Cypress Testing

--- Source/savefarmer/tests/e2e/specs/test.cy.ts
+++ Source/savefarmer/tests/e2e/specs/test.cy.ts
@@ -1,6 +1,24 @@
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/folder/Inbox')
-    cy.contains('#container', 'Inbox')
-  })
-})
+describe('App Navigation', () => {
+  it('loads the home page', () => {
+    cy.visit('/');
+    cy.contains('Button list'); // Checks if the list header renders
+  });
+
+  it('navigates to Profile page', () => {
+    cy.visit('/');
+    cy.contains('Profile').click();
+    cy.url().should('include', '/Profile');
+  });
+
+  it('navigates to Login page', () => {
+    cy.visit('/');
+    cy.contains('Login').click();
+    cy.url().should('include', '/Login');
+  });
+
+  it('navigates to Home page', () => {
+    cy.visit('/');
+    cy.contains('Home').click();
+    cy.url().should('include', '/Home');
+  });
+});
