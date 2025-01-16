69859cc00741f6efdeb37a62828d1c3de55e6d14
Eric Vo <tvo12@ewu.edu>
2025-01-16 10:54:53 -0800

    move posts function to it own ts script

--- Source/savefarmer/src/components/Board.vue
+++ Source/savefarmer/src/components/Board.vue
@@ -5,7 +5,7 @@
       <ion-card v-for="(post, index) in posts" :key="index" class="board-card">
         <ion-card-header>
           <ion-card-title>
-            <img :src="getCropIcon(post.crop_type)" alt="Crop Icon" class="crop-icon" />
+            <img :src="postSer.getCropIcon(post.crop_type)" alt="Crop Icon" class="crop-icon" />
             {{ post.title }}
           </ion-card-title>
         </ion-card-header>
@@ -22,7 +22,7 @@
             <span>${{ post.price }}</span>
             <span>{{ post.crop_type }}</span>
             <span>{{ post.vendor_name }}</span>
-            <span>{{ formatContact(post.contact) }}</span>
+            <span>{{ postSer.formatContact(post.contact) }}</span>
           </div>
           <!-- third line -->
           <div class="info-2details">
@@ -43,106 +43,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { IonCardTitle, IonCardHeader, IonCard, IonPage, IonCardContent, IonContent } from '@ionic/vue';
-import cornIcon from '@/images/icons/corn.png';
-import tomatoIcon from '@/images/icons/tomato.png';
-import wheatIcon from '@/images/icons/wheat.png';
-import carrotIcon from '@/images/icons/carrot.png';
-import cropIcon from '@/images/icons/crop.png';
-import potatoIcon from '@/images/icons/potato.png';
+import { postSer} from '@/scripts/PostService';
 
-interface Post {
-  title: string;
-  price: number;
-  crop_type: string;
-  amount: number;
-  location: string;
-  contact: string;
-  extra_info: string;
-  date: string;
-  expiry_date: string;
-  email: string;
-  vendor_name: string;
-}
-
-const posts = ref<Post[]>([
-  {
-    title: 'Fresh Potatoes for Sale. Organic and Locally Grown.',
-    price: 100,
-    crop_type: 'Potato',
-    amount: 100,
-    location: 'Spokane, WA, USA',
-    contact: '8496554654',
-    vendor_name: 'Red Barn Farms',
-    email: 'nowhere@somewhere.com',
-    extra_info: 'Extra Info',
-    date: '2021-09-01',
-    expiry_date: '2021-09-30',
-  },  
-  {
-    title: 'Carrots for Sale. Fresh and Organic.',
-    price: 200,
-    crop_type: 'Carrot',
-    amount: 200,
-    location: 'Seattle, WA, USA',
-    contact: '2156548231',
-    vendor_name: 'Blue Barn Farms',
-    email: 'somewhere@nowhere.com',
-    extra_info: 'Extra Info 2',
-    date: '2021-09-02',
-    expiry_date: '2021-09-29',
-  },
-  {
-    title: 'Rotten Tomatoes for Sale. Cheap and Expired.',
-    price: 10,
-    crop_type: 'Tomato',
-    amount: 200,
-    location: 'Seattle, WA, USA',
-    contact: '2424548231',
-    vendor_name: 'Brown Barn Farms',
-    email: 'somewhere2@nowhere.com',
-    extra_info: 'Extra Info 2',
-    date: '2021-09-02',
-    expiry_date: '2021-09-29',
-  },
-  {
-    title: 'Unripe Corn for Sale. Fresh and Raw.',
-    price: 900,
-    crop_type: 'Corn',
-    amount: 1,
-    location: 'Seattle, WA, USA',
-    contact: '9999999999',
-    vendor_name: 'Yellow Barn Farms',
-    email: 'nowhere2@nowhere.com',
-    extra_info: 'Extra Info 2',
-    date: '2021-09-02',
-    expiry_date: '2021-09-29',
-  },
-]);
-
-function formatContact(contact: string): string {
-  return contact.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
-}
-
-//icon from https://www.flaticon.com/ and need to put in Attribution
-//would be better if they all from the same pack but should work for now
-function getCropIcon(cropType: string): string {
-  switch (cropType.toLowerCase()) {
-    case 'corn':
-      return cornIcon;
-    case 'tomato':
-      return tomatoIcon;
-    case 'wheat':
-      return wheatIcon;
-    case 'carrot':
-      return carrotIcon;
-    case 'potato':
-      return potatoIcon;
-    default:
-      return cropIcon;
-  }
-}
+const posts = computed(() => postSer.posts.value);
 
 
 </script>
