577373907c57d998d1070bf89262f3caaa178243
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2025-02-17 14:43:30 -0800

    DB Desktop Bootup
    Installed DB on Desktop

--- Source/FarmerAPI/FarmerAPI/obj/Debug/net8.0/FarmerAPI.AssemblyInfo.cs
+++ Source/FarmerAPI/FarmerAPI/obj/Debug/net8.0/FarmerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106755f0ae5f980482c715fc25117865decbdaa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d020f263c621564da400a8583b7e313ce4d2531")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
