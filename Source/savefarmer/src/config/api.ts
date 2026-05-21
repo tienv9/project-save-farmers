c65ccc40d96e6275ccf616e9fc07fa796a785cc2
Eric Vo <tvo12@ewu.edu>
2026-05-20 22:56:17 -0700

    fixing issue

--- Source/savefarmer/src/config/api.ts
+++ Source/savefarmer/src/config/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://save-farmer-server-env.eba-spy2qq27.us-east-2.elasticbeanstalk.com/';
+export const API_BASE_URL = 'https://save-farmer-server-env.eba-spy2qq27.us-east-2.elasticbeanstalk.com';
